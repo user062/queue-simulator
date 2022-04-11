9c588bd91a0e37efddcbdf6d8b66eaea25c9923b
b <b@a.com>
2022-04-12 00:59:46 +0100

    made the linked list generic, and added cli arguments to simulation and some other improvments

--- simulation.c
+++ simulation.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "simulation.h"
 
-#define BIG_NUMBER 10000000
 
 Clients_Queue *processes_queue;
-Events_Queue *events_queue;
+Events_Queue events_queue;
 Server_state server;
 time_t t_now, service_time, arrival_time;
 
 float mean_response_time, mean_waiting_time;
-int clients_served, new_client_id;
+long clients_served, new_client_id, number_of_clients;
 
 void process_arrival() {
     Client *client = malloc(sizeof(Client));
@@ -45,8 +45,9 @@ void process_departure(Client *client) {
 }
 
 void print_statistics() {
-    printf("mean_response_time: %f\n", mean_response_time/clients_served);
-    printf("mean_waiting_time: %f\n", mean_waiting_time/clients_served);
+    printf("clients served: %ld\n", clients_served);
+    printf("mean response time: %f\n", mean_response_time/clients_served);
+    printf("mean waiting time: %f\n", mean_waiting_time/clients_served);
     printf("throughput: %f\n", (float)clients_served/t_now);
 }
 
@@ -68,10 +69,11 @@ void delete_event(Event *event) {
 void init() {
     server = idle;
     processes_queue = create_clients_queue();
-    events_queue = create_events_queue();
+    events_queue = *create_events_queue();
     t_now = 0;
     service_time = 5;
     arrival_time = 6;
+    number_of_clients = 10000000;
     mean_response_time = 0;
     mean_waiting_time = 0;
     clients_served = 0;
@@ -79,32 +81,51 @@ void init() {
     create_event(arrival, t_now);
 }
 
-void print_events() {
-    Events_Queue *events = events_queue;
 
-    while(events->next != NULL) {
-        switch(events->value->type) {
-        case arrival: printf("arrival -> "); break;
-        case start_service: printf("start_service -> "); break;
-        case departure: printf("departure -> "); break;
-        }
-        events = events->next;
-    }
-        switch(events->value->type) {
-        case arrival: printf("arrival\n"); break;
-        case start_service: printf("start_service\n"); break;
-        case departure: printf("departure\n"); break;
-        }
+bool detect_overflow() {
+    return
+        events_queue != NULL &&
+        ((Event*)events_queue->data)->type == departure &&
+        events_queue->next != NULL &&
+        ((Event*)events_queue->next->data)->type == start_service; 
 }
 
 int main(int argc, char *argv[])
 {
+    init();
+
+    if (argc == 2 && strcmp(argv[0], "--help")) {
+        printf("Simulator for D/D/1 Queue\n");
+        printf("Usage: simulator --help\n");
+        printf("Usage: print this message\n");
+        printf("Usage: simulator number_of_clients client_arrival_time client_service_time \n");
+        printf("Usage: simulator (without arguments, equivalent to simulator 10000000 6 5 ) \n");
+        return 0;
+    }
+
+    if (argc == 3) {
+        printf("Simulator for D/D/1 Queue\n");
+        printf("missing arguments\n");
+        printf("Usage: simulator --help\n");
+        printf("Usage: print the help message\n");
+        printf("Usage: simulator number_of_clients client_arrival_time client_service_time \n");
+        printf("Usage: simulator (without arguments, equivalent to simulator 10000000 6 5 ) \n");
+        return 0;
+    }
+
+    else if (argc == 4) {
+        number_of_clients = strtol(argv[1], NULL, 10);
+        arrival_time = strtol(argv[2], NULL, 10);
+        service_time = strtol(argv[3], NULL, 10);
+        
+    }
+
     Event *event;
     Client *client;
 
-    init();
-
-    while (clients_served <= BIG_NUMBER) {
+    while (clients_served < number_of_clients) {
+        print_events_queue(events_queue);
+        print_clients_queue(*processes_queue);
         event = get_event();
         t_now = event->time;
 
@@ -115,7 +136,6 @@ int main(int argc, char *argv[])
         }
 
         delete_event(event);
-
     }
 
     print_statistics();
